a920c03160a00a35b6970553b14776fbee73259d
Slava Shklyaev <shk.slava@gmail.com>
2015-05-15 01:18:42 +0300

    Reveal one door

--- app/Game.php
+++ app/Game.php
@@ -4,6 +4,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model {
 
+    protected $casts = [
+        'prize_door'     => 'integer',
+        'initial_choice' => 'integer',
+        'revealed_door'  => 'integer',
+        'final_choice'   => 'integer',
+    ];
+
     public static function boot() {
         parent::boot();
 
@@ -15,6 +22,10 @@ class Game extends Model {
     public function choose($door) {
         if (is_null($this->initial_choice)) {
             $this->initial_choice = $door;
+            $this->revealed_door = collect(range(1, 3))->reject(function($door) {
+                return $door === $this->prize_door ||
+                       $door === $this->initial_choice;
+            })->random();
         }
         else if (is_null($this->final_choice)) {
             $this->final_choice = $door;
