b047479a039314ea19f02c3924a1ec5a68a48580
Slava Shklyaev <shk.slava@gmail.com>
2015-05-15 21:20:08 +0300

    Use Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,8 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.less('app.less');
+    mix.scripts([
+        '../../../bower_components/jquery/dist/jquery.js',
+        '../../../bower_components/bootstrap/dist/js/bootstrap.js'
+    ], 'public/js/vendor.js');
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,37 @@
 <head>
     <meta charset="utf-8">
     <title>@yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Laravel</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/') }}">Home</a></li>
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ url('/auth/login') }}">Login</a></li>
+                    <li><a href="{{ url('/auth/register') }}">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     @yield('content')
+
+    <script src="{{ asset('/js/vendor.js') }}"></script>
 </body>
 </html>
