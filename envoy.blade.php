0bd5bf160cd2f91c67806e5bd7dacee4ed1436c0
Slava Shklyaev <shk.slava@gmail.com>
2015-05-16 01:39:48 +0300

    Set up deployment with Envoy

---
+++ envoy.blade.php
@@ -0,0 +1,29 @@
+@include('vendor/autoload.php')
+@setup
+    Dotenv::load('.', '.env');
+
+    $host     = env('DEPLOY_HOST');
+    $app_root = env('DEPLOY_APP_ROOT');
+@endsetup
+
+@servers(['host' => $host])
+
+@task('deploy', ['on' => 'host'])
+    set -ex
+    cd {{ $app_root }};
+
+    php artisan down
+
+    git pull
+    find storage -type d -exec chmod 777 {} +
+
+    ./composer.phar install --no-interaction --no-dev --prefer-dist --optimize-autoloader
+
+    php artisan migrate --force
+
+    npm install --quiet
+    ./node_modules/.bin/bower install --quiet
+    ./node_modules/.bin/gulp
+
+    php artisan up
+@endtask
