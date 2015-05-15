129cd8b1a9a06c1d173fdb977580f437a3e593c1
Slava Shklyaev <shk.slava@gmail.com>
2015-05-15 14:06:24 +0300

    Add GamesSeeder

---
+++ database/seeds/GamesSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Game;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class GamesSeeder extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        Model::unguard();
+        for ($i = 1; $i <= 300; ++$i) {
+            $game = Game::create([]);
+            if (static::chance(4, 5)) {
+                $game->choose(rand(1, 3));
+                if (static::chance(4, 5)) {
+                    $door = collect(range(1, 3))->filter(function($door) use ($game) {
+                        return $door !== $game->revealed_door;
+                    })->random();
+                    $game->choose($door);
+                }
+            }
+            $game->save();
+            echo $game . PHP_EOL;
+        }
+    }
+
+    protected static function chance($chance, $total_chance) {
+        return rand(1, $total_chance) <= $chance;
+    }
+}
