949fa16d5032ac327a609572bd883fa1d5cff4c7
Slava Shklyaev <shk.slava@gmail.com>
2015-05-15 22:02:15 +0300

    Use elixir() for cache busting

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,13 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
-    mix.less('app.less');
+    mix.less();
     mix.scripts([
         '../../../bower_components/jquery/dist/jquery.js',
-        '../../../bower_components/bootstrap/dist/js/bootstrap.js'
-    ], 'public/js/vendor.js');
+        '../../../bower_components/bootstrap/dist/js/bootstrap.js',
+    ], 'public/js/app.js');
+    mix.version([
+        'css/app.css',
+        'js/app.js',
+    ]);
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-default">
@@ -34,6 +34,6 @@
 
     @yield('content')
 
-    <script src="{{ asset('/js/vendor.js') }}"></script>
+    <script src="{{ elixir('js/app.js') }}"></script>
 </body>
 </html>
