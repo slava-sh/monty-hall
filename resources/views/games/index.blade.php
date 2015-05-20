6e55ed9d3a84b24287077886e077b72ffaf6dfd5
Slava Shklyaev <shk.slava@gmail.com>
2015-05-20 20:48:30 +0300

    Hide statistics

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,13 +1,15 @@
 @extends('app')
 
 @section('content')
-    <pre>
-{{ $played_game_count }} games played
-{{ sprintf('%5.2f', $win_percent)             }}% won
-{{ sprintf('%5.2f', $stay_percent)            }}% stayed
-{{ sprintf('%5.2f', $switch_percent)          }}% switched
-{{ sprintf('%5.2f', $win_percent_of_stayed)   }}% of those who stayed won
-{{ sprintf('%5.2f', $win_percent_of_switched) }}% of those who switched won
-    </pre>
+
+<!--
+    {{ $played_game_count }} games played
+    {{ sprintf('%5.2f', $win_percent)             }}% won
+    {{ sprintf('%5.2f', $stay_percent)            }}% stayed
+    {{ sprintf('%5.2f', $switch_percent)          }}% switched
+    {{ sprintf('%5.2f', $win_percent_of_stayed)   }}% of those who stayed won
+    {{ sprintf('%5.2f', $win_percent_of_switched) }}% of those who switched won
+-->
+
     @include('games.new-game-button', ['title' => 'Play'])
 @stop
