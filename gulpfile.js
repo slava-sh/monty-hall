457cc74e7f1ab33814f9fe88a03420c2fed3f780
Slava Shklyaev <shk.slava@gmail.com>
2015-05-22 04:28:07 +0300

    Store normalize.css locally

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,15 @@ elixir.config.sourcemaps = false;
 
 elixir(function(mix) {
     mix.less();
+    mix.styles([
+        'bower_components/normalize.css/normalize.css',
+        'public/css/app.css',
+    ], 'public/css/all.css', './');
     mix.scripts([
         'app.js',
     ], 'public/js/app.js');
     mix.version([
-        'css/app.css',
+        'css/all.css',
         'js/app.js',
     ]);
 });
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css" rel="stylesheet">
-    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:400,700,600" rel="stylesheet">
+    <link href="{{ elixir('css/all.css') }}" rel="stylesheet">
 </head>
 <body id="{{ $page_id }}">
     <div class="container">@yield('content')</div>
